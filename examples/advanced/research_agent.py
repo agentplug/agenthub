0b454a17c7a573875412dc0ca8c78289772b38ce
nguyennm <nguyennm1024@gmail.com>
2025-11-26 18:52:47 -0800

    chore(examples): update research agent query

--- examples/advanced/research_agent.py
+++ examples/advanced/research_agent.py
@@ -4,6 +4,6 @@ research_agent = ah.load_agent(
     "agentplug/research-agent", external_tools=["web_search"]
 )
 
-query = "Current war in Cambodia"
+query = "Who is the current president of US"
 result = research_agent.instant_research(query)
 print(result)
